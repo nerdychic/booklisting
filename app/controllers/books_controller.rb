8a727d240a61b2a8505a5dca6972ade4957f498a
nerdychic <nanapplesun@gmail.com>
2015-12-07 00:27:41 +0800

    Add books/users association

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /books
   # GET /books.json
@@ -14,7 +16,7 @@ class BooksController < ApplicationController
 
   # GET /books/new
   def new
-    @book = Book.new
+    @book = current_user.books.build
   end
 
   # GET /books/1/edit
@@ -24,7 +26,7 @@ class BooksController < ApplicationController
   # POST /books
   # POST /books.json
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.build(book_params)
 
       if @book.save
         redirect_to @book, notice: 'Book was successfully created.' 
@@ -58,8 +60,13 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
     end
 
+  def correct_user
+      @book = current_user.books.find_by(id: params[:id])
+      redirect_to books_path, notice: "Not authorized to edit this book" if @book.nil?
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:description)
+      params.require(:book).permit(:description, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203150326) do
+ActiveRecord::Schema.define(version: 20151205152148) do
 
   create_table "books", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "books", ["user_id"], name: "index_books_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
